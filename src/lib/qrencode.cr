b4cc4d49e68c81ab86a06e4f4c039907d4b4b93c
Jack Thorne <jack@kilmer.co>
2019-07-03 17:05:17 -0500

    working encoding and printing

--- src/lib/qrencode.cr
+++ src/lib/qrencode.cr
@@ -1,6 +1,15 @@
 module Lib
   @[Link("qrencode")]
   lib QREncode
+    type QRinput = Void*
+
+    struct QRcode
+      version : LibC::Int
+      width : LibC::Int
+      data : UInt8*
+    end
+
+    fun QRcode_encodeInput(input : QRinput) : QRcode*
     enum QRecLevel
       QrEclevelL = 0
       QrEclevelM = 1
@@ -8,32 +17,37 @@ module Lib
       QrEclevelH = 3
     end
     enum QRencodeMode
-      QrModeNul = -1
-      QrModeNum = 0
-      QrModeAn = 1
-      QrMode8 = 2
-      QrModeKanji = 3
-      QrModeStructure = 4
-      QrModeEci = 5
-      QrModeFnc1First = 6
-      QrModeFnc1Second = 7
-    end
-    struct QRcode
-      version : LibC::Int
-      width : LibC::Int
-      data : UInt8*
+      QrModeNul        = -1
+      QrModeNum        =  0
+      QrModeAn         =  1
+      QrMode8          =  2
+      QrModeKanji      =  3
+      QrModeStructure  =  4
+      QrModeEci        =  5
+      QrModeFnc1First  =  6
+      QrModeFnc1Second =  7
     end
     fun QRcode_encodeString(string : LibC::Char*, version : LibC::Int, level : QRecLevel, hint : QRencodeMode, casesensitive : LibC::Int) : QRcode*
     fun QRcode_encodeString8bit(string : LibC::Char*, version : LibC::Int, level : QRecLevel) : QRcode*
+    fun QRcode_encodeStringMQR(string : LibC::Char*, version : LibC::Int, level : QRecLevel, hint : QRencodeMode, casesensitive : LibC::Int) : QRcode*
+    fun QRcode_encodeString8bitMQR(string : LibC::Char*, version : LibC::Int, level : QRecLevel) : QRcode*
     fun QRcode_encodeData(size : LibC::Int, data : UInt8*, version : LibC::Int, level : QRecLevel) : QRcode*
-    type QRinput = Void*
-    fun QRinput_new : QRinput
-    fun QRinput_append(input : QRinput, mode : QRencodeMode, size : LibC::Int, data : UInt8*) : LibC::Int
-    fun QRcode_encodeInput(input : QRinput) : QRcode*
+    fun QRcode_encodeDataMQR(size : LibC::Int, data : UInt8*, version : LibC::Int, level : QRecLevel) : QRcode*
     fun QRcode_free(qrcode : QRcode*)
     type QRinputStruct = Void*
-    fun QRinput_Struct_new : QRinputStruct
-    fun QRinput_Struct_appendInput(s : QRinputStruct, input : QRinput) : LibC::Int
-    fun QRinput_Struct_insertStructuredAppendHeaders(s : QRinputStruct) : LibC::Int
+
+    struct X_QRcodeList
+      code : QRcode*
+      next : X_QRcodeList*
+    end
+
+    type QRcodeList = X_QRcodeList
+    fun QRcode_encodeInputStructured(s : QRinputStruct) : QRcodeList*
+    fun QRcode_encodeStringStructured(string : LibC::Char*, version : LibC::Int, level : QRecLevel, hint : QRencodeMode, casesensitive : LibC::Int) : QRcodeList*
+    fun QRcode_encodeString8bitStructured(string : LibC::Char*, version : LibC::Int, level : QRecLevel) : QRcodeList*
+    fun QRcode_List_size(qrlist : QRcodeList*) : LibC::Int
+    fun QRcode_List_free(qrlist : QRcodeList*)
+    fun QRcode_APIVersion(major_version : LibC::Int*, minor_version : LibC::Int*, micro_version : LibC::Int*)
+    fun QRcode_clearCache
   end
 end
